1f601ec9a0ae8cfae0d94e8b19429e66d9ab0004
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-04-14 09:59:01 +0600

    footer section is added in assignemt-07 keenkeeper

--- assignment-07/src/Layout/MainLayout.jsx
+++ assignment-07/src/Layout/MainLayout.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import { Outlet } from "react-router";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
     <>
-    <Header/>
-    <Outlet></Outlet>
-    <Footer/>
+      <div className="container mx-auto">
+        <Header />
+        <Outlet></Outlet>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- assignment-07/src/components/Footer.jsx
+++ assignment-07/src/components/Footer.jsx
@@ -1,7 +1,44 @@
+import { GrInstagram } from "react-icons/gr";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-[#244D3F] text-white text-center">
+      <h1 className="text-5xl font-bold pt-20 pb-4">KeenKeeper</h1>
+      <p className="text-[16px]">
+        <small>
+          Your personal shelf of meaningful connections. Browse, tend, and
+          nurture the relationships that matter most.
+        </small>
+      </p>
+      <p className="text-[20px] font-medium pt-6 pb-4">Social Links</p>
+      <div className="flex justify-center items-center gap-3 text-black pb-10">
+        <div className="bg-white p-2 rounded-full">
+          <GrInstagram />
+        </div>
+        <div className="bg-white p-2 rounded-full">
+          <FaFacebookSquare />
+        </div>
+        <div className="bg-white p-2 rounded-full">
+          <FaXTwitter />
+        </div>
+      </div>
+      <div className="divider container mx-auto my-divider"></div>
+      <div className="flex flex-col md:flex-row justify-between items-center container mx-auto pb-8">
+        <p>
+          <small className="text-[#ffffffe5]">
+            © 2026 KeenKeeper. All rights reserved.
+          </small>
+        </p>
+        <div className="flex justify-center items-center gap-6">
+          <p className="text-[#ffffffe5]">Privacy Policy</p>
+          <p className="text-[#ffffffe5]">Terms of Service</p>
+          <p className="text-[#ffffffe5]">Cookies</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
