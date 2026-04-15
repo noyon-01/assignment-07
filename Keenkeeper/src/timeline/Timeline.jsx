3d3b648ccf9dacf0a314912f367f8a9461ed3ce5
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-04-15 23:16:53 +0600

    sorting functionality is added in assignment 7

--- Keenkeeper/src/timeline/Timeline.jsx
+++ Keenkeeper/src/timeline/Timeline.jsx
@@ -1,6 +1,6 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../context/UserContext";
-import { IoMdArrowDropdown } from "react-icons/io";
+import { MdArrowDropDown } from "react-icons/md";
 import callImg from "../assets/call.png";
 import textImg from "../assets/text.png";
 import videoImg from "../assets/video.png";
@@ -11,11 +11,35 @@ const Timeline = () => {
   const totalData =
     storedCallData.length + storedTextData.length + storedVideoData.length;
 
+  const [filter, setFilter] = useState("all");
+
   return (
     <div className="w-9/12 py-20 mx-auto">
       <h1 className="text-5xl font-bold mb-6">Timeline</h1>
 
       <div className="dropdown dropdown-start mb-6">
+        <div className="dropdown dropdown-start">
+          <div tabIndex={0} role="button" className="btn m-1 text-[#64748B]">
+            Filter timeline <MdArrowDropDown />
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a onClick={() => setFilter("all")}>All</a>
+            </li>
+            <li>
+              <a onClick={() => setFilter("call")}>Call</a>
+            </li>
+            <li>
+              <a onClick={() => setFilter("text")}>Text</a>
+            </li>
+            <li>
+              <a onClick={() => setFilter("video")}>Video</a>
+            </li>
+          </ul>
+        </div>
       </div>
 
       {totalData === 0 ? (
@@ -27,65 +51,53 @@ const Timeline = () => {
         </div>
       ) : (
         <div className="space-y-4">
-          <div className="space-y-4">
-            {storedCallData.map((call) => (
+          {(filter === "all" || filter === "call") &&
+            storedCallData.map((call) => (
               <div
-                className="bg-white shadow p-6 rounded flex items-center gap-2.5"
                 key={call.id}
+                className="bg-white shadow p-6 rounded flex items-center gap-2.5"
               >
                 <img src={callImg} alt={call.name} />
                 <div>
-                  <h1 className="text-lg text-[#64748B]">
-                    <strong className="text-xl font-semibold text-black">
-                      Call
-                    </strong>{" "}
-                    with {call.name}
+                  <h1>
+                    <strong>Call</strong> with {call.name}
                   </h1>
-                  <h1>{call.next_due_date}</h1>
+                  <p>{call.next_due_date}</p>
                 </div>
               </div>
             ))}
-          </div>
 
-          <div className="space-y-4">
-            {storedTextData.map((text) => (
+          {(filter === "all" || filter === "text") &&
+            storedTextData.map((text) => (
               <div
-                className="bg-white shadow p-4 rounded flex items-center gap-2.5"
                 key={text.id}
+                className="bg-white shadow p-4 rounded flex items-center gap-2.5"
               >
                 <img src={textImg} alt={text.name} />
                 <div>
-                  <h1 className="text-lg text-[#64748B]">
-                    <strong className="text-xl font-semibold text-black">
-                      Text
-                    </strong>{" "}
-                    with {text.name}
+                  <h1>
+                    <strong>Text</strong> with {text.name}
                   </h1>
-                  <h1>{text.next_due_date}</h1>
+                  <p>{text.next_due_date}</p>
                 </div>
               </div>
             ))}
-          </div>
 
-          <div className="space-y-4">
-            {storedVideoData.map((video) => (
+          {(filter === "all" || filter === "video") &&
+            storedVideoData.map((video) => (
               <div
-                className="bg-white shadow p-6 rounded flex items-center gap-2.5"
                 key={video.id}
+                className="bg-white shadow p-6 rounded flex items-center gap-2.5"
               >
                 <img src={videoImg} alt={video.name} />
                 <div>
-                  <h1 className="text-lg text-[#64748B]">
-                    <strong className="text-xl font-semibold text-black">
-                      Video
-                    </strong>{" "}
-                    with {video.name}
+                  <h1>
+                    <strong>Video</strong> with {video.name}
                   </h1>
-                  <h1>{video.next_due_date}</h1>
+                  <p>{video.next_due_date}</p>
                 </div>
               </div>
             ))}
-          </div>
         </div>
       )}
     </div>
